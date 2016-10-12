53eb2d451807b7f9b10eedb48a44de21e7fd52c3
hehaichi <hehaichi@gmail.com>
2016-10-12 13:07:00 +0530

    Login and logout working

--- landing/urls.py
+++ landing/urls.py
@@ -6,4 +6,5 @@ from django.contrib.auth.views import login
 urlpatterns = [
     url(r'^$',views.home, name='home'),
     url(r'^login/',login,{'template_name':'login.html','authentication_form': LoginForm},name='login'),
+    url(r'^logout/',views.home_logout,name='logout')
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
+from django.contrib.auth import logout
 # Create your views here.
 from django.http import HttpResponse
 app_name='landing'
@@ -9,3 +10,7 @@ def home(request):
 		return render(request,"home.html")
 	else:
 		return redirect('login/')
+
+def home_logout(request):
+	logout(request)
+	return render(request,'logout.html')

b9ff4a348e61212c94047cb7b3bdfe590a2f6cd1
hehaichi <hehaichi@gmail.com>
2016-10-12 12:22:52 +0530

    Fixes

--- events/views.py
+++ events/views.py
@@ -24,4 +24,4 @@ def view_attendees(request,urlhash):
 	json=e.attending.all()
 	#print json
 	ser=serializers.serialize('json',json,fields=('name','profession', 'college', 'email'))
-	return HttpResponse({"results":str(ser)})
+	return HttpResponse(str({"results":str(ser)}))

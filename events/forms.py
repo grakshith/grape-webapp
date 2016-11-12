c82119b5bfd672cce8bf13896500b56a5a62f120
hehaichi <hehaichi@gmail.com>
2016-11-13 02:56:49 +0530

    Fix

--- events/forms.py
+++ events/forms.py
@@ -1,15 +1,12 @@
 from  django import forms
 from .models import Event
-class DateInput(forms.DateTimeInput):
-    input_type = 'date'
-class TimeInput(forms.DateTimeInput):
-    input_type = 'time'
+from django.contrib.admin import widgets as wdgt
 class EventForm(forms.ModelForm):
     class Meta:
         model=Event
         fields=['name','date','attending','image']
         widgets = {
-            'date': DateInput(),
-            'time':TimeInput(),
+            'date': wdgt.AdminSplitDateTime()
         }
+        
         

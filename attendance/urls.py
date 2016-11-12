a44f2601b918763de4e21c3b7679f98d819856c3
hehaichi <hehaichi@gmail.com>
2016-11-12 22:56:49 +0530

    Fix

--- attendance/urls.py
+++ attendance/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += patterns("",
+    urlpatterns +=(
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True }),
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
